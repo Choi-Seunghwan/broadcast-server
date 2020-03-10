f45fc68e036521a975ef1cd6cfbe9dc5243c4c58
ChoiSeunghwan-childly <csh@childly.com>
2020-03-10 21:01:39 +0900

    Choi-Seunghwan

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
-import express from 'express';
+import express from "express";
+import io from "socket.io";
 
 const app = express();
+
+
 const server = app.listen(3005, () => {
-  console.log('express server has started on port 3005');
-})
+  console.log("express server has started on port 3005");
+});
