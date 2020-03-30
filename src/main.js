6c814381067dfb0e6e1cc73d6c7d087971a44ea4
Choi-Seunghwan <choiseunghwan.tech@gmail.com>
2020-03-30 18:57:40 +0900

    set prettier

--- src/main.js
+++ src/main.js
@@ -1,23 +1,23 @@
 const app = require("express")();
 const http = require("http").createServer(app);
-const io = require('socket.io')(http);
+const io = require("socket.io")(http);
 
 app.get("/", function(req, res) {
   res.send("<h1>Hello socket</h1>");
 });
 
-io.on('connection', (socket) => {
-  console.log('a user connected');
+io.on("connection", socket => {
+  console.log("a user connected");
 
-  socket.on('chat message', () => {
-    console.log('message: ' + msg);
-  })
+  socket.on("chat", () => {
+    console.log("message: " + msg);
+  });
 
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
-  })
-})
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
 
-http.listen(3000, function() {
-  console.log('listen : 3000');
+http.listen(3002, function() {
+  console.log("listen : 3002");
 });
