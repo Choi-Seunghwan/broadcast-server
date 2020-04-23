4715b7c5fe485e4348cdc5b7ff44341b0097d6e9
Choi-Seunghwan <choiseunghwan.tech@gmail.com>
2020-04-23 20:28:06 +0900

    frontend와 socket 통신 테스트 중

--- src/main.js
+++ src/main.js
@@ -1,23 +1,31 @@
-const app = require("express")();
-const http = require("http").createServer(app);
-const io = require("socket.io")(http);
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
-app.get("/", function(req, res) {
-  res.send("<h1>Hello socket</h1>");
+let count = 0;
+
+app.get('/', function (req, res) {
+  res.send('<h1>Hello socket</h1>');
 });
 
-io.on("connection", socket => {
-  console.log("a user connected");
+io.on('connection', (socket) => {
+  console.log('a user connected');
 
-  socket.on("chat", () => {
-    console.log("message: " + msg);
+  socket.on('chat', () => {
+    console.log('message: ' + msg);
   });
 
-  socket.on("disconnect", () => {
-    console.log("user disconnected");
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
   });
+
+  setInterval(() => {
+    count++;
+    socket.emit('test', count);
+    console.log('test, ', count);
+  }, 1000);
 });
 
-http.listen(3002, function() {
-  console.log("listen : 3002");
+http.listen(3002, function () {
+  console.log('listen : 3002');
 });
