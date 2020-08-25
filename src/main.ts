9481ff0ee6a9436fcdbad49578b165aef7c3c6b7
Choi-Seunghwan <choiseunghwan.tech@gmail.com>
2020-08-26 08:45:50 +0900

    server class 생성

--- src/main.ts
+++ src/main.ts
@@ -1,37 +1,7 @@
-import express from 'express';
+import { Server } from './server';
 
-const app = express();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
-const port = 3002;
+const server = new Server();
 
-let count = 0;
-
-app.get('/', function (req, res) {
-  res.send('<h1>Hello socket</h1>');
+server.listen((port) => {
+  console.log(`Server is listening on http://localhost:${port}`);
 });
-
-io.on('connection', (socket) => {
-  console.log('a user connected');
-
-  socket.on('chat', (msg) => {
-    console.log('message: ' + msg);
-  });
-
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
-  });
-
-  setInterval(() => {
-    count++;
-    socket.emit('test', count);
-    console.log('test, ', count);
-  }, 1000);
-});
-
-http.listen(port, function () {
-  console.log(`listen : ${port}`);
-});
-
-
-module.exports = {app}
