8e0a694527b44e68029356d2f81c07fe847628da
Choi Seunghwan <49161475+Choi-Seunghwan@users.noreply.github.com>
2021-06-09 20:45:21 +0900

    [VS] nodemon local, dev 구분

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { Server } from './server';
 
 export const server = new Server();
 
+console.log('@@@', process.env.NODE_ENV);
+
 server.listen((port) => {
   console.log(`Server is listening on http://localhost:${port}`);
 });
