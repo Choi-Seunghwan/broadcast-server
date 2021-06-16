9943a8fbd628102353072e2f36fa7037ddba222e
Choi Seunghwan <49161475+Choi-Seunghwan@users.noreply.github.com>
2021-06-17 00:44:18 +0900

    1. [VS] Local debug 환경 설정 2. [VS] sequelize local 설정

--- src/libs/dbConnector.ts
+++ src/libs/dbConnector.ts
@@ -1,8 +1,14 @@
 import Logger from '@/utils/logger';
+
 const Sequelize = require('sequelize');
+const { DataTypes } = require('sequelize');
+const dbConfig = require('@/db/dbConfig.json');
+
+const SEQ_MODEL_ACCOUNT = 'account';
 
 class DbConnector {
   private sequelizeInstance;
+  private accountModel;
 
   constructor() {
     this.init();
@@ -12,18 +18,50 @@ class DbConnector {
     const connectionCheck = async () => {
       try {
         this.sequelizeInstance.authenticate();
+
+        this.initModel();
       } catch (e) {
         Logger.debug('@@@ catch', e);
       }
     };
-
-    if (process.env.NODE_ENV === 'local') {
-      //
-    }
-    this.sequelizeInstance = new Sequelize('sqlite::memory:');
+    // if (process.env.NODE_ENV === 'local') {
+    //   //
+    // }
+    this.sequelizeInstance = new Sequelize(dbConfig[process.env.NODE_ENV]);
     connectionCheck();
 
     Logger.debug('@@ dbConnector init end');
+  }
+
+  async initModel() {
+    const initAccountModel = () => {
+      this.accountModel = sq.define(SEQ_MODEL_ACCOUNT, {
+        id: {
+          type: DataTypes.INTEGER,
+          primaryKey: true,
+          allowNull: false,
+        },
+        nickname: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+        password: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+        createDateTime: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+        },
+        updateDateTime: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+        },
+      });
+    };
+
+    const sq = this.sq();
+    initAccountModel();
   }
 
   sq() {
